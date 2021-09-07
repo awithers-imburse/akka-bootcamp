69080da15610d6a18fcd8691eae9e9dfc18f214a
Andrew Withers <andrew.withers@imbursepayments.com>
2021-09-07 10:17:42 +0100

    Unit 1: Replace raw string messages with message classes and restructure code with mock input validator

--- src/Unit-1/DoThis/ConsoleReaderActor.cs
+++ src/Unit-1/DoThis/ConsoleReaderActor.cs
@@ -9,7 +9,9 @@ namespace WinTail
     /// </summary>
     class ConsoleReaderActor : UntypedActor
     {
+        public const string StartCommand = "start";
         public const string ExitCommand = "exit";
+
         private IActorRef _consoleWriterActor;
 
         public ConsoleReaderActor(IActorRef consoleWriterActor)
@@ -19,20 +21,69 @@ namespace WinTail
 
         protected override void OnReceive(object message)
         {
-            var read = Console.ReadLine();
-            if (!string.IsNullOrEmpty(read) && string.Equals(read, ExitCommand, StringComparison.OrdinalIgnoreCase))
+            if (message.Equals(StartCommand))
             {
-                // shut down the system (acquire handle to system via
-                // this actors context)
-                Context.System.Terminate();
-                return;
+                DoPrintInstructions();
+            }
+            else if (message is Messages.InputError)
+            {
+                _consoleWriterActor.Tell(message as Messages.InputError);
             }
 
-            // send input to the console writer to process and print
-            _consoleWriterActor.Tell(read);
+            GetAndValidateInput();
+        }
 
-            // continue reading messages from the console
-            Self.Tell("continue");
+        private void DoPrintInstructions()
+        {
+            Console.WriteLine("Write whatever you want into the console!");
+            Console.WriteLine("Some entries will pass validation, and some won't...\n\n");
+            Console.WriteLine($"Type '{ExitCommand}' to quit this application at any time.\n");
+        }
+
+        /// <summary>
+        /// Reads input from console, validates it, then signals appropriate response
+        /// (continue processing, error, success, etc).
+        /// </summary>
+        private void GetAndValidateInput()
+        {
+            var message = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(message))
+            {
+                // signal that user needs to supply an input, as previously received input was blank
+                Self.Tell(new Messages.NullInputError("No input received."));
+            }
+            else if (string.Equals(message, ExitCommand, StringComparison.OrdinalIgnoreCase))
+            {
+                // shut down the entire actor system (allows the process to exit)
+                Context.System.Terminate();
+            }
+            else
+            {
+                var isValid = IsValid(message);
+
+                if (isValid)
+                {
+                    _consoleWriterActor.Tell(new Messages.InputSuccess("Message was valid."));
+
+                    Self.Tell(new Messages.ContinueProcessing());
+                }
+                else
+                {
+                    Self.Tell(new Messages.ValidationError("Invalid: input had an odd number of characters."));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Validates <see cref="message"/>.
+        /// Currently says messages are valid if contain an even number of characters.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        private bool IsValid(string message)
+        {
+            return message.Length % 2 == 0;
         }
     }
 }
